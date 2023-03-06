d146a679b066544e86441ef173f8b4ce30f5b8fe
KonstantinS343 <mihailrudkovic@gmail.com>
2023-03-06 17:31:20 +0300

    name

--- lab2/logic.py
+++ lab2/logic.py
@@ -6,7 +6,7 @@ class LogicFunction:
     def __init__(self) -> None:
         self.logic_formula: str = None
         self.table_object = PrettyTable()
-        self.table_instance = []
+        self.table_data = []
         self.perfect_conjunctive_normal_form_formula: str = ''
         self.perfect_disjunctive_normal_form_formula: str = ''
         self.perfect_conjunctive_normal_form_in_bynary = []
@@ -30,7 +30,7 @@ class LogicFunction:
     def create_logic_table(self):
         self.table_object.field_names = ['x1', 'x2', 'x3', 'f(sknf)', 'f(sdnf)', 'i']
         value_of_x1_x2_x3 = product(range(2), repeat=3)
-        const = 128
+        begin_index = 128
         
         for i in value_of_x1_x2_x3:
             self.temp_logic_formula = self.logic_formula
@@ -39,18 +39,18 @@ class LogicFunction:
             self.temp_logic_formula = self.temp_logic_formula.replace('x3', str(i[2]))
             self.table_object.add_row([i[0], i[1], i[2], int(eval(self.temp_logic_formula)), 
                                        int(eval(self.temp_logic_formula)), 
-                                       const,])
-            self.table_instance.append({
+                                       begin_index,])
+            self.table_data.append({
                 'x1':i[0],
                 'x2':i[1],
                 'x3':i[2],
                 'f':int(eval(self.temp_logic_formula)),
-                'i':const,
+                'i':begin_index,
             })
-            const //=2 
+            begin_index //=2 
 
     def perfect_conjunctive_normal_form(self):
-        for i in self.table_instance:
+        for i in self.table_data:
             if i['f'] == 0:
                 part_of_form = ' + '.join([values*'!'+keys for keys, values in i.items() if keys != 'f' and keys!= 'i'])
                 self.perfect_conjunctive_normal_form_formula += f'( {part_of_form} )*'
@@ -58,13 +58,15 @@ class LogicFunction:
         self.perfect_conjunctive_normal_form_in_number_form()
         
     def perfect_conjunctive_normal_form_in_number_form(self):
-        temp = self.perfect_conjunctive_normal_form_formula.split()
-        for i in range(0,len(temp)):
-            if temp[i] == 'x1' or temp[i]=='x2' or temp[i] == 'x3':
+        temp_perfect_conjunctive_normal = self.perfect_conjunctive_normal_form_formula.split()
+        for i in range(0,len(temp_perfect_conjunctive_normal)):
+            if temp_perfect_conjunctive_normal[i] == 'x1' or temp_perfect_conjunctive_normal[i]=='x2' \
+                or temp_perfect_conjunctive_normal[i] == 'x3':
                 self.perfect_conjunctive_normal_form_in_bynary.append('0')
-            elif temp[i] == '!x1' or temp[i] == '!x2' or temp[i]  == '!x3':
+            elif temp_perfect_conjunctive_normal[i] == '!x1' or temp_perfect_conjunctive_normal[i] == '!x2' \
+                or temp_perfect_conjunctive_normal[i]  == '!x3':
                 self.perfect_conjunctive_normal_form_in_bynary.append('1')
-            elif temp[i][0] == ')':
+            elif temp_perfect_conjunctive_normal[i][0] == ')':
                 self.perfect_conjunctive_normal_form_in_bynary.append('*')
         self.perfect_conjunctive_normal_form_in_bynary = ''.join(self.perfect_conjunctive_normal_form_in_bynary[:-1])
         self.perfect_conjunctive_normal_form_in_decimal = self.translate_in_decimal(self.perfect_conjunctive_normal_form_in_decimal,
@@ -86,7 +88,7 @@ class LogicFunction:
         return object    
     
     def perfect_disjunctive_normal_form(self):
-        for i in self.table_instance:
+        for i in self.table_data:
             if i['f'] == 1:
                 part_of_form = ' * '.join([abs(values-1)*'!'+keys for keys, values in i.items() if keys != 'f' and keys!= 'i'])
                 self.perfect_disjunctive_normal_form_formula += f'( {part_of_form} )+'
@@ -94,13 +96,15 @@ class LogicFunction:
         self.perfect_disjunctive_normal_form_in_number_form()
         
     def perfect_disjunctive_normal_form_in_number_form(self):
-        temp = self.perfect_disjunctive_normal_form_formula.split()
-        for i in range(0,len(temp)):
-            if temp[i] == 'x1' or temp[i]=='x2' or temp[i] == 'x3':
+        temp_perfect_disjunctive_normal = self.perfect_disjunctive_normal_form_formula.split()
+        for i in range(0,len(temp_perfect_disjunctive_normal)):
+            if temp_perfect_disjunctive_normal[i] == 'x1' or temp_perfect_disjunctive_normal[i]=='x2' \
+                or temp_perfect_disjunctive_normal[i] == 'x3':
                 self.perfect_disjunctive_normal_form_in_bynary.append('1')
-            elif temp[i] == '!x1' or temp[i] == '!x2' or temp[i]  == '!x3':
+            elif temp_perfect_disjunctive_normal[i] == '!x1' or temp_perfect_disjunctive_normal[i] == '!x2' \
+                or temp_perfect_disjunctive_normal[i]  == '!x3':
                 self.perfect_disjunctive_normal_form_in_bynary.append('0')
-            elif temp[i][0] == ')':
+            elif temp_perfect_disjunctive_normal[i][0] == ')':
                 self.perfect_disjunctive_normal_form_in_bynary.append('+')
         self.perfect_disjunctive_normal_form_in_bynary = ''.join(self.perfect_disjunctive_normal_form_in_bynary[:-1])
         self.perfect_disjunctive_normal_form_in_decimal = self.translate_in_decimal(self.perfect_disjunctive_normal_form_in_decimal,
@@ -109,7 +113,7 @@ class LogicFunction:
         self.perfect_disjunctive_normal_form_in_decimal = '+('+', '.join(self.perfect_disjunctive_normal_form_in_decimal)+')'
         
     def calculate_ratio(self):
-        for i in self.table_instance:
+        for i in self.table_data:
             if i['f'] == 1:
                 self.number_ratio += i['i'] 
         
