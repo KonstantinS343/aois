fac656335d777bd9484ee9e2c496a4fb25aae8aa
KonstantinS343 <mihailrudkovic@gmail.com>
2023-03-07 18:51:40 +0300

    readme

--- lab2/main.py
+++ lab2/main.py
@@ -4,11 +4,13 @@ from logic_operation import LogicFunction
 def main():
     logic_function = LogicFunction()
     # logic_function.handler_input_formula('!((!x1+!x3)*!(!x2*!x3))')
-    logic_function.handler_input_formula(input('Input formula: '))
+    function = input('Input formula: ')
+    logic_function.handler_input_formula(function)
     logic_function.create_logic_table()
     logic_function.perfect_conjunctive_normal_form()
     logic_function.perfect_disjunctive_normal_form()
     logic_function.calculate_ratio()
+    print(f'Function: {function}')
     print(f'Table:\n{logic_function.table_object}')
     print(
         f'Perfect conjunctive normal form: {logic_function.perfect_conjunctive_normal_form_formula}')
