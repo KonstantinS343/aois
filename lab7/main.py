7faed209892397e9ff8777268761806949b46540
KonstantinS343 <mihailrudkovic@gmail.com>
2023-05-13 11:36:11 +0300

    added search

--- lab7/main.py
+++ lab7/main.py
@@ -2,13 +2,26 @@ from memory import Memory
 
 def main():
     memory = Memory([3, 67, 12, 33, 0, 1, 2, 23])
+    user_input = ' '
     memory()
-    print('\nSort in ascending order:')
-    memory.sort(False)
-    memory()
-    print('\nSort in descending order:')
-    memory.sort(True)
-    memory()
+    while user_input:
+        user_input = input('1 - sort;\n2 - find;\n')
+        match user_input:
+            case '1':
+                print('\nSort in ascending order:')
+                memory.sort(False)
+                memory()
+                print('\nSort in descending order:')
+                memory.sort(True)
+                memory()
+            case '2':
+                mask = input('Enter mask(use "x" instead spaces): ')
+                print('\nFind by mask: 0xx0xxx1')
+                memory.search_by_mask('0xx0xxx1')
+                print(f'\nFind by mask: {mask}')
+                memory.search_by_mask(mask)
+            case _:
+                break
 
 if __name__=='__main__':
     main()
--- lab7/memory.py
+++ lab7/memory.py
@@ -1,5 +1,7 @@
+from typing import List
+
 class Memory:
-    def __init__(self, inputs):
+    def __init__(self, inputs: List[int]) -> None:
         self.memory = []
         
         for i in inputs:
@@ -9,7 +11,7 @@ class Memory:
         for i in self.memory:
             print(i)
     
-    def translate_to_binary(self, decimal_number):
+    def translate_to_binary(self, decimal_number: int) -> List[int]:
         binary_number = []
         while decimal_number:
             binary_number.append(decimal_number%2)
@@ -20,7 +22,7 @@ class Memory:
         
         return list(reversed(binary_number))
     
-    def translate_to_decimal(self):
+    def translate_to_decimal(self) -> List[int]:
         decimal_number = []
         temp_decimal_number = 0
         
@@ -31,7 +33,7 @@ class Memory:
             temp_decimal_number = 0
         return decimal_number
     
-    def comparison(self, first_word, second_word, flag):
+    def comparison(self, first_word: List[int], second_word: List[int], flag: bool) -> bool:
         g_variable, l_variable = 0, 0
         previous_g_variable, previous_l_variable = 0, 0
         
@@ -45,9 +47,37 @@ class Memory:
         elif l_variable:
             return not flag
     
-    def sort(self, sort_flag):
+    def sort(self, sort_flag: bool) -> None:
         for i in range(len(self.memory)):
             for j in range(len(self.memory)-1-i):
                 if self.comparison(self.memory[j], self.memory[j+1], sort_flag):
                     self.memory[j], self.memory[j+1] = self.memory[j+1], self.memory[j]
-        print(str(self.translate_to_decimal())+'\n')        
+        print(str(self.translate_to_decimal())+'\n')    
+        
+    def split_mask(self, mask: str) -> List[int|str]:
+        mask_split = []
+        for i in mask:
+            try:
+                mask_split.append(int(i))
+            except ValueError:
+                mask_split.append(i)
+        
+        return mask_split
+        
+    def search_by_mask(self, mask: str) -> List[List[int]]:
+        mask = self.split_mask(mask)
+        search_result = []    
+        
+        for i in self.memory:
+            search_find = True
+            for j in range(len(i)):
+                if not ((mask[j] == 1 and i[j]) or (mask[j] == 0 and not i[j]) or mask[j] == 'x'):
+                    search_find = False
+                    break
+            if search_find:
+                search_result.append(i)
+        if search_result:
+            for i in search_result:
+                print(i)
+        else:
+            print('No matches!')
